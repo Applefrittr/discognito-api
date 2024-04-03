c67082c5071ac726194992391b44563b63c91001
applefrittr <jacob.lee.jordan@gmail.com>
2024-04-03 16:50:19 -0400

    socket connect/disconnect logs, code cleanup

--- events/discord-events.js
+++ events/discord-events.js
@@ -5,7 +5,6 @@ const { io } = require("../bin/server");
 const discordEvents = (client) => {
   // Discognito Bot listener that fires when a new message is posted into a channel that the bot has permission
   client.on(Events.MessageCreate, async (message) => {
-    console.log(message);
     // forward message to sockets currently in the specifc channelId Room
     io.sockets
       .to(message.channelId)
--- events/socket-events.js
+++ events/socket-events.js
@@ -1,5 +1,5 @@
 // helper funtion to attach listeners to socket connection
-const socketEvents = (socket) => {
+const socketEvents = (socket, io) => {
   // join the passed channel id Room
   socket.on("join channel", (id) => {
     socket.join(id);
@@ -8,6 +8,12 @@ const socketEvents = (socket) => {
   // leave the channel id Room (2nd room in the socket.rooms Set)
   socket.on("leave channel", () => {
     socket.leave(Array.from(socket.rooms)[1]);
+  });
+
+  // log socket disconnection and current connected socket count
+  socket.on("disconnect", () => {
+    console.log("Discognito client disconnected :/");
+    console.log("Current connections:", io.sockets.sockets.size);
   });
 };
 
